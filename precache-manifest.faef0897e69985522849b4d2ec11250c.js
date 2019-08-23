0703c8607be85874c3cff724edcc37aaa47e2015
agieselm <gieselman.austin@gmail.com>
2019-08-23 17:43:30 -0500

    Updates

---
+++ precache-manifest.faef0897e69985522849b4d2ec11250c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc38c40ba3ee12e766aa450bfb79154d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "afaa4c194c370a4b1be5",
+    "url": "/monster-rolodex/static/css/main.b60cfed2.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "afaa4c194c370a4b1be5",
+    "url": "/monster-rolodex/static/js/main.101a6150.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
